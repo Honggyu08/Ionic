08796e2a2930318d971e9f663b42486aa3870ad7
GeunHo Kim <kgh30312@gmail.com>
2021-07-26 08:22:08 +0900

    possible

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,24 +4,40 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: HomePage,
-  },  
+    children: [
+      {
+        path: 'ideas',
+        loadChildren: () => import('./ideas/ideas.module').then( m => m.IdeasPageModule)
+      },
+      {
+        path: 'solution',
+        loadChildren: () => import('./solution/solution.module').then( m => m.SolutionPageModule)
+      },
+      {
+        path: 'home-tab',
+        loadChildren: () => import('./home-tab/home-tab.module').then( m => m.HomeTabPageModule)
+      },
+      {
+        path: 'diary',
+        loadChildren: () => import('./diary/diary.module').then( m => m.DiaryPageModule)
+      },
+      {
+        path: 'store',
+        loadChildren: () => import('./store/store.module').then( m => m.StorePageModule)
+      },
+      {
+        path: '',
+        redirectTo: 'home/home-tab',
+        pathMatch: 'full'
+      }
+    ]
+  },   
   {
-    path: 'ideas',
-    loadChildren: () => import('../ideas/ideas.module').then(m => m.IdeasPageModule)
-  },
-  {
-    path: 'solution',
-    loadChildren: () => import('../solution/solution.module').then(m => m.SolutionPageModule)
-  },
-  {
-    path: 'diary',
-    loadChildren: () => import('../diary/diary.module').then(m => m.DiaryPageModule)
-  },
-  {
-    path: 'store',
-    loadChildren: () => import('../store/store.module').then(m => m.StorePageModule)
+    path: '',
+    redirectTo: 'home/home-tab',
+    pathMatch: 'full'
   }
 ];
 
