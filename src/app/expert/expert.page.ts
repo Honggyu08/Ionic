78885302b578317e6001e5988e08980a16c8c9b1
honggyu <6398lhg@gmail.com>
2021-12-23 00:08:14 +0900

    Revert "Revert "확인 팝업 창 네, 아니요 순서 조정""
    This reverts commit 83e6e21afd3e6df4ad965d6ab5248f5db8297da4.

--- src/app/expert/expert.page.ts
+++ src/app/expert/expert.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-expert',
@@ -8,31 +7,9 @@ import { AlertController } from '@ionic/angular';
 })
 export class ExpertPage implements OnInit {
 
-  constructor(public alertController: AlertController) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  async applyClicked() {
-    const alert = await this.alertController.create({
-      header: 'Apply',
-      message: '신청하시겠습니까?',
-      buttons: [
-        {
-          text: '네',
-          handler: () => {}
-        },
-        {
-          text: '아니요',
-          role: 'cancel',
-          handler: () => {}
-        }
-      ]
-    });
-
-    await alert.present();
-    
-    const button = document.querySelector('.save');
-    button.addEventListener('click', this.applyClicked);
-  }
 }
