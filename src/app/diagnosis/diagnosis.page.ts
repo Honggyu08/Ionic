08f4f788ae2777e27ec098cd4bf59e8a645d5516
Honggyu <6398lhg@gmail.com>
2022-01-03 02:00:42 +0900

    카메라 인터페이스

--- src/app/diagnosis/diagnosis.page.ts
+++ src/app/diagnosis/diagnosis.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-diagnosis',
@@ -8,9 +9,16 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 })
 export class DiagnosisPage implements OnInit {
 
+  selectedImage: any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  checkPlatformForWeb() {
+    if(Capacitor.getPlatform() == 'web') return true;
+    return false;
   }
 
   async takePicture() {
@@ -20,16 +28,34 @@ export class DiagnosisPage implements OnInit {
       //allowEditing: true,
       source: CameraSource.Prompt,
 
-      resultType: CameraResultType.Uri
+      resultType: this.checkPlatformForWeb() ? CameraResultType.DataUrl : CameraResultType.Uri // Uri 보통 app에서, DataUrl 브라우져
     });
 
-    // image.webPath will contain a path that can be set as an image src.
-    // You can access the original file using image.path, which can be 
-    // passed to the Filesystem API to read the raw data of the image,
-    // if desired (or pass resultType: CameraResultType.base64 to getPhoto)
-    var imageUrl = image.webPath;
-
-    // Can be set to the src of an image now
-    // imageElement.src = imageUrl;
+    this.selectedImage = image;
+    if(this.checkPlatformForWeb()) this.selectedImage.webPath = image.dataUrl;
   }
 }
+
+/*
+ios
+  <key>NSCameraUsageDescription</key>
+  <string>To Take Photos and Video</string>
+  <key>NSPhototLibraryAddUsageDescription</key>
+  <string>Store camera photos to camera</string>
+  <key>NSPhotoLibraryUsageDescription</key>
+  <string>Need library access to upload images</string>
+*/
+/*
+ionic capacitor add android
+capacitor.config.json에서 appID 바꾸기 (ex]edu.skku.skin)
+위에 안 해도 될 듯
+AndroidManifest.xml에 이거 추가
+<uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+variables.gradle.file에 
+androidxExifInterfaceVersion = '1.3.2'
+androidxMaterialVersion = '1.3.0'
+ionic cap sync android
+ionic cap open android
+build apk
+*/
