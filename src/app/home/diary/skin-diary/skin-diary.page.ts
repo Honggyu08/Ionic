de9e7df7d539e91f7d91470ad37ef404722f694a
Honggyu <6398lhg@gmail.com>
2022-01-07 17:35:13 +0900

    skin diary 페이지 진단페이지 연결

--- src/app/home/diary/skin-diary/skin-diary.page.ts
+++ src/app/home/diary/skin-diary/skin-diary.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-skin-diary',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkinDiaryPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  diagnosis() {
+    this.router.navigate(['/diagnosis']);
+  }
 }
