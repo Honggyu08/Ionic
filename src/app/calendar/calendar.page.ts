a209c81a502f3cef35cee41b774d76167232379a
honggyu <6398lhg@gmail.com>
2022-01-14 02:49:15 +0900

    모달 수정

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { CalendarComponent } from 'ionic2-calendar';
+import { IonDatetime } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,11 +13,20 @@ export class CalendarPage implements OnInit {
   eventSource = [];
   viewTitle: string;
 
+  dataValue2 = '';
   calendar = {
     mode: 'month',
     currentDate: new Date()
   };
 
+  event = {
+    titie: '',
+    desc: '',
+    time: null,
+    allDay:true
+  };
+
+  @ViewChild(IonDatetime, {static: true }) datetime: IonDatetime;
   @ViewChild(CalendarComponent) myCal: CalendarComponent;
   
   constructor(private modalCtrl: ModalController, private router: Router) { }
@@ -35,4 +45,48 @@ export class CalendarPage implements OnInit {
   onViewTitleChanged(title) {
     this.viewTitle = title;
   }
+
+  save() {
+    // this.datetime.nativeEl.save();
+    this.modalCtrl.dismiss({event: this.event});
+  }
+
+  dateChanged(value) {
+    console.log(value);
+  }
+
+  // async openCalModal() {
+  //   const modal = await this.modalCtrl.create({
+  //     component: CalendarPage,
+  //     cssClass: 'calendar',
+  //     backdropDismiss: false
+  //   });
+
+  //   await modal.present();
+
+  //   modal.onDidDismiss().then((result) => {
+  //     if (result.data && result.data.event) {
+  //       let event = result.data.event;
+  //       if (event.allDay) {
+  //         let start = event.startTime;
+  //         event.startTime = new Date(
+  //           Date.UTC(
+  //             start.getUTCFullYear(),
+  //             start.getUTCMonth(),
+  //             start.getUTCDate()
+  //           )
+  //         );
+  //         event.endTime = new Date(
+  //           Date.UTC(
+  //             start.getUTCFullYear(),
+  //             start.getUTCMonth(),
+  //             start.getUTCDate() + 1
+  //           )
+  //         );
+  //       }
+  //       this.eventSource.push(result.data.event);
+  //       this.myCal.loadEvents();
+  //     }
+  //   });
+  // }
 }
